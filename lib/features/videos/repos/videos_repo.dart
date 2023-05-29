8c7f40406b1e4cede7b2413902075b393ef0617a
Kim <wow4090@gmail.com>
2023-05-30 02:11:08 +0900

    0530_k_4

--- lib/features/videos/repos/videos_repo.dart
+++ lib/features/videos/repos/videos_repo.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tiktok_clone/features/videos/models/video_like_model.dart';
 import 'package:tiktok_clone/features/videos/models/video_model.dart';
 
 class VideosRepository {
@@ -11,39 +12,52 @@ class VideosRepository {
 
   UploadTask uploadVideoFile(File video, String uid) {
     final fileRef = _storage.ref().child(
-          "/videos/$uid/${DateTime.now().millisecondsSinceEpoch.toString()}",
-        );
+        '/videos/$uid/${DateTime.now().millisecondsSinceEpoch.toString()}');
     return fileRef.putFile(video);
   }
 
   Future<void> saveVideo(VideoModel data) async {
-    await _db.collection("videos").add(data.toJson());
+    await _db.collection('videos').add(data.toJson());
   }
 
-  Future<QuerySnapshot<Map<String, dynamic>>> fetchVideos({
-    int? lastItemCreatedAt,
-  }) {
+  Future<QuerySnapshot<Map<String, dynamic>>> fetchVideos(
+      {int? lastItemCreatedAt}) async {
     final query = _db
         .collection("videos")
         .orderBy("createdAt", descending: true)
         .limit(2);
-    if (lastItemCreatedAt == null) {
-      return query.get();
-    } else {
-      return query.startAfter([lastItemCreatedAt]).get();
-    }
+
+    return lastItemCreatedAt == null
+        ? await query.get()
+        : await query.startAfter([lastItemCreatedAt]).get();
   }
 
-  Future<void> likeVideo(String videoId, String userId) async {
+  Future<VideoLikeModel> isLiked(String videoId, String userId) async {
+    final likeQuery = _db.collection("likes").doc("${videoId}000$userId");
+    final videoQuery = _db.collection("videos").doc(videoId);
+
+    final like = await likeQuery.get();
+    final video = await videoQuery.get();
+    final videoData = video.data();
+
+    final VideoModel vm =
+        VideoModel.fromJson(json: videoData!, videoId: videoId);
+
+    return VideoLikeModel(isLikeVideo: like.exists, likeCount: vm.likes);
+  }
+
+  Future<bool> likeVideo(String videoId, String userId) async {
     final query = _db.collection("likes").doc("${videoId}000$userId");
     final like = await query.get();
 
     if (!like.exists) {
-      await query.set(
-        {
-          "createdAt": DateTime.now().millisecondsSinceEpoch,
-        },
-      );
+      await query.set({
+        "createdAt": DateTime.now().millisecondsSinceEpoch,
+      });
+      return false;
+    } else {
+      await query.delete();
+      return true;
     }
   }
 }
--- lib/features/videos/view_models/video_post_view_models.dart
+++ lib/features/videos/view_models/video_post_view_models.dart
@@ -1,26 +1,41 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tiktok_clone/features/authentication/repos/authentication_repo.dart';
+import 'package:tiktok_clone/features/videos/models/video_like_model.dart';
 import 'package:tiktok_clone/features/videos/repos/videos_repo.dart';
 
-class VideoPostViewModel extends FamilyAsyncNotifier<void, String> {
+class VideoPostViewModel extends FamilyAsyncNotifier<VideoLikeModel, String> {
   late final VideosRepository _repository;
   late final String _videoId;
+  late final String _userId;
+  bool _isLiked = false;
+  int _likeCounts = 0;
 
   @override
-  FutureOr<void> build(String arg) {
-    _videoId = arg;
+  FutureOr<VideoLikeModel> build(String arg) {
+    final argStr = arg.split("000");
+    _videoId = argStr[0];
+    _userId = argStr[1];
     _repository = ref.read(videosRepo);
+    return isLikedVideo();
+  }
+
+  Future<VideoLikeModel> isLikedVideo() async {
+    final likeData = await _repository.isLiked(_videoId, _userId);
+    _isLiked = likeData.isLikeVideo;
+    _likeCounts = likeData.likeCount;
+    return likeData;
   }
 
   Future<void> likeVideo() async {
-    final user = ref.read(authRepo).user;
-    await _repository.likeVideo(_videoId, user!.uid);
+    final isLiked = await _repository.likeVideo(_videoId, _userId);
+    _isLiked = !isLiked;
+    _likeCounts = isLiked ? _likeCounts - 1 : _likeCounts + 1;
+    state = AsyncValue.data(
+        VideoLikeModel(isLikeVideo: _isLiked, likeCount: _likeCounts));
   }
 }
 
 final videoPostProvider =
-    AsyncNotifierProvider.family<VideoPostViewModel, void, String>(
-  () => VideoPostViewModel(),
-);
+    AsyncNotifierProvider.family<VideoPostViewModel, VideoLikeModel, String>(
+        () => VideoPostViewModel());
